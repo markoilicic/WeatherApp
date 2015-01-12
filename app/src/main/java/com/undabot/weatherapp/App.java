c56a5291f2291a5e1b5802d68a2c725fcbac102c
Marko Ilicic <soso.the.man@gmail.com>
2015-01-12 21:07:11 +0100

    Added permissions for internet and acces ntwrk state, added okhttpclient for caching responses

--- app/src/main/java/com/undabot/weatherapp/App.java
+++ app/src/main/java/com/undabot/weatherapp/App.java
@@ -1,20 +1,29 @@
 package com.undabot.weatherapp;
 
 import android.app.Application;
+import android.content.Context;
 
 import timber.log.Timber;
 
 
 public class App extends Application {
 
+    private static Context mAppContext;
+
     @Override
     public void onCreate() {
         super.onCreate();
+
+        mAppContext = getApplicationContext();
 
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
         } else {
             Timber.plant(new Timber.HollowTree());
         }
+    }
+
+    public static Context getAppContext() {
+        return mAppContext;
     }
 }
--- app/src/main/java/com/undabot/weatherapp/data/api/ApiServiceManager.java
+++ app/src/main/java/com/undabot/weatherapp/data/api/ApiServiceManager.java
@@ -1,14 +1,27 @@
 package com.undabot.weatherapp.data.api;
 
+import android.app.Application;
+
+import com.squareup.okhttp.Cache;
+import com.squareup.okhttp.OkHttpClient;
+import com.undabot.weatherapp.App;
+
+import java.io.File;
+
 import retrofit.RestAdapter;
+import retrofit.client.OkClient;
+import timber.log.Timber;
 
 public class ApiServiceManager {
+
+    private static final int HTTP_DISC_CACHE_SIZE = 10 * 1024 * 1024; //10MB
 
     private ApiService mApiService;
 
     public ApiServiceManager() {
         RestAdapter restAdapter = new RestAdapter.Builder()
                 .setEndpoint(ApiConstants.BASE_URL)
+                .setClient(new OkClient(createOkHttpClient((Application) App.getAppContext())))
                 .setRequestInterceptor(new ApiHeader())
                 .setLogLevel(RestAdapter.LogLevel.FULL)
                 .build();
@@ -20,6 +33,21 @@ public class ApiServiceManager {
         return mApiService;
     }
 
-    //TODO create okHttp client for caching response
+    /**
+     * Create okHttpClient for caching responses
+     */
+    public static OkHttpClient createOkHttpClient(Application application) {
+        OkHttpClient okHttpClient = new OkHttpClient();
+
+        try {
+            File discCache = new File(application.getCacheDir(), "http");
+            Cache cache = new Cache(discCache, HTTP_DISC_CACHE_SIZE);
+            okHttpClient.setCache(cache);
+        } catch (Exception e) {
+            Timber.e(e, "Unable to install disk cache");
+        }
+
+        return okHttpClient;
+    }
 
 }
